9916c7f1558d7a66c1b99a502567033b7f5ff322
wlo2398219 <wlo2398219@gmail.com>
2017-10-06 23:17:27 +0800

    Add costs.py

--- scripts/costs.py
+++ scripts/costs.py
@@ -1,3 +1,4 @@
+<<<<<<< e188284032459f1acf48519b3d0d8ea2223f45f7
 import numpy as np
 
 def calculate_mse(e):
@@ -18,3 +19,33 @@ def compute_loss(y, tx, w):
     e = y - tx.dot(w)
     return calculate_mse(e)
     # return calculate_mae(e)
+=======
+# -*- coding: utf-8 -*-
+"""Function used to compute the loss."""
+
+import numpy as np
+
+# *******************
+# General Setting
+# y: N x 1 vector
+# tx: N x D matrix
+# w: D x 1 vector
+# *******************
+
+def compute_loss(y, tx, w):
+ 
+    v_error = y - tx.dot(w)
+    error = np.inner(v_error, v_error)/(2 * y.shape[0]) 
+    return error
+ 
+
+def compute_mse(y, tx, w):
+
+    v_error = y - tx.dot(w)
+    v_error = y - y_expect
+    error = np.inner(v_error, v_error)/(2 * y.shape[0])
+    return error
+
+def compute_rmse(y, tx, w):
+    return np.sqrt(2*compute_mse(y, tx, w))
+>>>>>>> Add costs.py
