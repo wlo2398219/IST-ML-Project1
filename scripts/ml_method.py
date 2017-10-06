e188284032459f1acf48519b3d0d8ea2223f45f7
wlo2398219 <wlo2398219@gmail.com>
2017-10-06 23:09:17 +0800

    Add ML_Method Scripts

---
+++ scripts/ml_method.py
@@ -0,0 +1,27 @@
+
+import numpy as np
+from costs import *
+
+# Linear regression using gradient descent
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    pass
+
+# Linear regression using stochastic gradient descent
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+    pass
+
+# Least squares regression using normal equations
+def least_squares(y, tx):
+    pass
+
+# Ridge regression using normal equations
+def ridge_regression(y, tx, lambda_):
+    pass
+
+# Logistic regression using gradient descent or SGD
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    pass
+
+# Regularized logistic regression using gradient descent or SGD
+def reg_logistic_regression(y, tx, initial_w, max_iters, gamma):
+    pass
