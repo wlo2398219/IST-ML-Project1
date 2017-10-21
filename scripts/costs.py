fdb767165c65538369d2af0d649bcf2c0f3e9106
sanadhis <sanadhis@gmail.com>
2017-10-21 21:02:45 +0200

    Fix Costs.py

--- scripts/costs.py
+++ scripts/costs.py
@@ -16,6 +16,22 @@ def compute_mse(y, tx, w):
     mse         = np.inner(value_error, value_error)/(2 * y.shape[0]) 
     return mse
 
+def compute_mae(y, tx, w):
+    """Function to compute mean absolute error (MAE).
+
+    Args:
+        y  (numpy array): Matrix output of size N x 1.
+        tx (numpy array): Matrix input of size N x D.
+        w  (numpy array): Matrix weight (parameters of the model) of size D x 1.
+
+    Returns:
+        mae (float, scalar) : The mean absolute error (MAE) for given model w.
+    """
+    
+    value_error = y - tx.dot(w)
+    mae         = np.sum(np.fabs(value_error))/(y.shape[0])
+    return mae
+
 def compute_rmse(y, tx, w):
     """Function to compute root mean square error (RMSE).
 
