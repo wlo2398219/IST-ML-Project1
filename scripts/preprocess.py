7368147084ede31996045593dfe38a56a7abbe9d
wlo2398219 <wlo2398219@gmail.com>
2017-10-27 12:16:46 +0200

    @@

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -43,7 +43,7 @@ def standardize_with_median(x, with_ones = False):
     return stand_x
 
 
-def standardize_with_power_terms(x, power, with_ones = True, impute_with = 'mean', with_sqrt = False):
+def standardize_with_power_terms(x, power, with_ones = True, with_log = False):
     mask = (x != -999)
 
     for i in range(x.shape[1]):
@@ -53,7 +53,7 @@ def standardize_with_power_terms(x, power, with_ones = True, impute_with = 'mean
     std_dev = np.std(x, axis = 0)
     stand_x = (x - mean) / std_dev
 
-    if with_sqrt:
+    if with_log:
         for i in range(x.shape[1]):
             # if all(num > 0 for num in stand_x[:,i]):
             min_ = np.min(x[:, i])
@@ -86,32 +86,44 @@ def standardize_with_power_terms(x, power, with_ones = True, impute_with = 'mean
     return stand_x
 
 
-def standardize_with_power_terms1(x, power, with_ones = True, impute_with = 'mean', with_sqrt = False):
-    
-    min_ = np.min(x, axis = 0)
-    max_ = np.max(x, axis = 0)
-    stand_x = (x - min_) / max_
+def standardize_with_power_terms1(x, power, with_ones = True, with_log = False, cross_terms = False):
 
-    if with_sqrt:
+    mean = np.mean(x, axis = 0)
+    std_dev = np.std(x, axis = 0)
+    stand_x = (x - mean) / std_dev
+
+    if with_log:
         for i in range(x.shape[1]):
-            x_sqrt = 1 / (1 + np.log(1 + stand_x[:,i]))
+            min_ = np.min(x[:, i])
+            x_sqrt = 1/(1+np.log(1 + x[:,i] - min_))
             x_sqrt = (x_sqrt - np.mean(x_sqrt))/np.std(x_sqrt)
             stand_x = np.concatenate((stand_x,x_sqrt.reshape([x.shape[0],1])),1)
-            x_sqrt = np.log(1 + stand_x[:,i])
+            x_sqrt = np.log(1 + x[:,i] - min_)
             x_sqrt = (x_sqrt - np.mean(x_sqrt))/np.std(x_sqrt)
             stand_x = np.concatenate((stand_x,x_sqrt.reshape([x.shape[0],1])),1)
 
-    x_tmp = stand_x
-    for deg in range(2, power + 1):
-        x_tmp = x_tmp * stand_x
+    if cross_terms:
+    	for i in range(x.shape[1]):
+    		for j in range(i, x.shape[1]):
+    			cross = x[:, i] * x[:, j]
+    			cross = (cross - np.mean(cross)) / np.std(cross)
+    			stand_x = np.concatenate((stand_x,cross.reshape([x.shape[0],1])),1)
+    x_tmp = x
+	
+    if cross_terms == False:
+        start = 2
+    else:
+        start = 3
+
+    for deg in range(start, power + 1):
+        x_tmp = x_tmp * x
         x_sq = x_tmp
 
-        min_ = np.min(x_sq, axis = 0)
-        max_ = np.max(x_sq, axis = 0)
-        x_sq = (x_sq - min_) / max_
-
+        mean = np.mean(x_sq, axis = 0)
+        std_dev = np.std(x_sq, axis = 0)
+        x_sq = (x_sq - mean)/std_dev
+            
         stand_x = np.concatenate((stand_x,x_sq),axis = 1)
-
 
     # ---------- Add ones to the matrix --------
     if with_ones:
